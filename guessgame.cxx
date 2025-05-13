f5c3e520ad7acdabd1d605bf0a21b2877718b4fd
Abayon Greg <gregabayon23@gmail.com>
2025-05-13 23:06:58 +0800

    guessgame.cxx

--- guessgame.cxx
+++ guessgame.cxx
@@ -1,51 +1,44 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class GuessingGame {
 private:
-    string name, level;
-    int answer;
+    string name;
+    string levels[3] = {"Easy", "Medium", "Hard"};
+    int answers[3] = {5, 25, 500};
 
 public:
     void greetPlayer() {
         cout << "What's your name? ";
         cin >> name;
-        cout << "Hello: " << name << endl << endl;
-    }
-
-    bool selectLevel() {
-        cout << "Please select a level (Easy, Medium, Hard): ";
-        cin >> level;
-
-        if (level == "Easy") {
-            answer = 5;
-        } else if (level == "Medium") {
-            answer = 25;
-        } else if (level == "Hard") {
-            answer = 500;
-        } else {
-            cout << "Invalid level selected!" << endl;
-            return false;
-        }
-        return true;
+        cout << "Hello, " << name << "!" << endl << endl;
     }
 
     void play() {
-        int guess;
-        cout << "Guess the number: ";
-        while (true) {
-            cin >> guess;
-            if (guess < answer) {
-                cout << "TRY AGAIN! HIGHER" << endl;
-            } else if (guess > answer) {
-                cout << "TRY AGAIN! LOWER" << endl;
-            } else {
-                cout << "CORRECT! The number was " << answer << "." << endl;
-                break;
+        for (int round = 0; round < 3; ++round) {
+            cout << "\n--- LEVEL: " << levels[round] << " ---" << endl;
+            cout << "Guess the number!" << endl;
+
+            int guess;
+            int answer = answers[round];
+
+            while (true) {
+                cout << "Your guess: ";
+                cin >> guess;
+
+                if (guess < answer) {
+                    cout << "TRY AGAIN! HIGHER" << endl;
+                } else if (guess > answer) {
+                    cout << "TRY AGAIN! LOWER" << endl;
+                } else {
+                    cout << "CORRECT! The number was " << answer << "." << endl;
+                    break;
+                }
             }
         }
+
+        cout << "\nGreat job, " << name << "! You completed all levels!" << endl;
     }
 };
 
@@ -55,16 +48,14 @@ int main() {
     // Game notes and creators
     cout << "Created by: Greg Abayon, Arwil Araneta, Anito Bacog, Anica Martinez, John Rei Orienda" << endl;
     cout << "\nIMPORTANT NOTES:\n";
-    cout << "I. Easy is 1-9, Medium is 10-99, Hard is 100-999!" << endl;
-    cout << "II. For selecting a level always start in Uppercase (Ex. Easy not easy)" << endl;
+    cout << "I. Level Order: Easy → Medium → Hard" << endl;
+    cout << "II. Each level has a fixed number to guess!" << endl;
+    cout << "- Easy: 1 to 9\n- Medium: 10 to 99\n- Hard: 100 to 999" << endl;
     cout << "*********************************************************" << endl;
 
     GuessingGame game;
     game.greetPlayer();
-
-    if (game.selectLevel()) {
-        game.play();
-    }
+    game.play();
 
     return 0;
 }
